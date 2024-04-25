3fbdeefc3253e55515122ace3f0e3bcadf297b7f
Hunter-137 <asrarkulovamal@gmail.com>
2024-04-25 15:20:51 +0500

    feat: add onCompleate animations

--- task-1/js/index.js
+++ task-1/js/index.js
@@ -24,31 +24,99 @@ document.addEventListener("DOMContentLoaded", function () {
     leftImgItem;
   console.log(calcHeight);
 
+  const contentDescWrapper = Array.from(document.querySelectorAll(".main-content-desc-wrapper"));
+  const contentDescLeft = Array.from(document.querySelectorAll(".main-content-desc-left"));
+  const contentDescRight = Array.from(document.querySelectorAll(".main-content-desc-right"));
+  console.log(contentDescWrapper);
+  console.log(contentDescRight);
+
+  const onCompleteBg = document.querySelectorAll(".main-content-desc-left__bg");
+
   gsap.registerPlugin(ScrollTrigger);
+
+  const introScale = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".main-content-intro",
+      // markers: true,
+      start: "top bottom",
+      end: "top+=50% bottom",
+      scrub: true,
+    }
+  });
+
+  introScale.to(".main-content-intro__item", {
+    scale: 1,
+  })
+  
+  introScale.to(".main-content-intro", {
+    scale: 1,
+  },"<")
+
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".main-content",
       scrub: true,
       markers: true,
       start: "top top",
-      end: "bottom top",
+      end: "bottom+=3000vh top",
       pin: true,
+      ease: "none",
     },
   });
+
+  // contentDescWrapper.forEach(block, item => {
+  //   tl.to(block, {
+  //     xPercent: -100,
+  //   })
+
+  // });
 
   tl.to(".main-content-desc-wrapper._1", {
     xPercent: -100,
   });
 
-  tl.to(".main-content-desc-right", {
+  tl.to(".main-content-desc-left__item._1", {
+    clipPath: "inset(0 0 0 0%)",
+    scale: 1,
+  }, "<");
+
+  tl.to(".main-content-desc-right._1", {
     y: -calcHeight,
+    onComplete: () => {
+      onCompleteBg[0].classList.add("_active");
+    },
+    onReverseComplete: () => {
+      onCompleteBg[0].classList.remove("_active");
+    }
   });
 
   tl.to(".main-content-desc-wrapper._2", {
     xPercent: -100,
   });
 
-  tl.to(".main-content-desc-right", {
+  tl.to(".main-content-desc-left__item._2", {
+    clipPath: "inset(0 0 0 0%)",
+    scale: 1,
+  }, "<");
+
+  tl.to(".main-content-desc-right._2", {
     y: -calcHeight,
+    onComplete: () => {
+      onCompleteBg[1].classList.add("_active");
+      contentDescLeft[1].classList.add("_close");
+    },
+    onReverseComplete: () => {
+      onCompleteBg[1].classList.remove("_active");
+      contentDescRight[1].classList.remove("_close");
+    }
   });
+
+  // tl.to(".main-content-desc-right._2", {
+  //   onComplete: () => {
+  //     contentDescLeft[1].classList.add("_close");
+  //   },
+  //   onLeave: () => {
+  //     contentDescRight[1].classList.remove("_close");
+  //   }
+  // });
 });

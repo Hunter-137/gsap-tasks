375ac3c146169dcfe5570925a03c5f7156a61293
Hunter-137 <asrarkulovamal@gmail.com>
2024-04-29 15:56:59 +0500

    fix: change height on markers in gsap #3

--- task-1/js/index.js
+++ task-1/js/index.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", function () {
       scrub: true,
       // markers: true,
       start: "top top",
-      end: "bottom+=5000vh top",
+      end: "bottom+=10000vh top",
       pin: true,
       ease: "none",
     },

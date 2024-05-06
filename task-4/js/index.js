94a53060a113b98b88916ca47024337b79324857
Hunter-137 <asrarkulovamal@gmail.com>
2024-05-07 00:30:50 +0500

    feat: add clip-path, add text under the image, add gsap animation at clip-path

--- task-4/js/index.js
+++ task-4/js/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const locationList = Array.from(
+    document.querySelectorAll(".main-circle-list-items")
+  );
+  const locationImgWrapper = Array.from(
+    document.querySelectorAll(".main-circle-item__wrapper")
+  );
+  const locationMinutes = Array.from(
+    document.querySelectorAll(".main-circle-item__road-time")
+  );
+  const locationTextName = Array.from(
+    document.querySelectorAll(".main-circle-item__text")
+  );
+
   gsap.registerPlugin(ScrollTrigger);
-})
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".main",
+      markers: true,
+      start: "top top",
+      end: "bottom top",
+      pin: true,
+      scrub: 1,
+    },
+  });
+
+  tl.to(locationImgWrapper[0], {
+    clipPath: "circle(15% at 50% 50%)",
+  });
+
+  tl.to(locationMinutes[0], {
+    opacity: 1,
+  });
+
+  tl.to(
+    locationTextName[0],
+    {
+      opacity: 1,
+    },
+    "<"
+  );
+});

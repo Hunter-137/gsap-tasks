36587ac036a40ed276e336956976aacdbb7b6370
Hunter-137 <asrarkulovamal@gmail.com>
2024-05-10 19:08:56 +0500

    feat: add animation at first two circles

--- task-4/js/index.js
+++ task-4/js/index.js
@@ -2,8 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const locationList = Array.from(
     document.querySelectorAll(".main-circle-list-items")
   );
+  const locationListWithoutFirst = locationList.slice(1);
   const locationImgWrapper = Array.from(
     document.querySelectorAll(".main-circle-item__wrapper")
+  );
+  const locationImg = Array.from(
+    document.querySelectorAll(".main-circle-item__img")
+  );
+  const locationImgHiderBg = Array.from(
+    document.querySelectorAll(".main-circle-item__hider-bg")
   );
   const locationMinutes = Array.from(
     document.querySelectorAll(".main-circle-item__road-time")
@@ -16,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".main",
-      markers: true,
+      // markers: true,
       start: "top top",
       end: "bottom top",
       pin: true,
@@ -25,7 +32,20 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   tl.to(locationImgWrapper[0], {
-    clipPath: "circle(15% at 50% 50%)",
+    clipPath: "circle(15% at 50% 40%)",
+    duration: 2,
+  });
+
+  tl.to(
+    locationImg[0],
+    {
+      objectPosition: "50% -50%",
+    },
+    "<60%"
+  );
+
+  tl.to(locationImg[0], {
+    scale: 0.85,
   });
 
   tl.to(locationMinutes[0], {
@@ -39,4 +59,55 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     "<"
   );
+
+  tl.to(locationListWithoutFirst, {
+    yPercent: -55,
+  })
+
+  tl.to(locationImgWrapper[0], {
+    clipPath: "circle(0% at 50% 50%)",
+    duration: 2,
+  });
+
+  tl.to(
+    locationImgWrapper[0],
+    {
+      yPercent: -50,
+      duration: 2,
+    },
+    "<"
+  );
+
+  tl.to(
+    locationMinutes[0],
+    {
+      opacity: 0,
+      yPercent: -10,
+    },
+    "<"
+  );
+
+  tl.to(
+    locationTextName[0],
+    {
+      opacity: 0,
+      yPercent: -10,
+    },
+    "<"
+  );
+
+  tl.to(locationList[1], {
+    yPercent: -100,
+    duration: 2,
+  }, "<");
+
+  tl.to(locationImgHiderBg[1], {
+    clipPath: "circle(0% at 50% 50%)",
+  }, "<");
+
+  tl.to(locationImgWrapper[1], {
+    clipPath: "circle(15% at 50% 50%)",
+    duration: 2,
+  }, "<");
+  
 });

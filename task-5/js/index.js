a09da9a511e7f5847beb7d81a4e5bba9f0666a3d
Hunter-137 <asrarkulovamal@gmail.com>
2024-05-22 19:02:54 +0500

    feat: add scrollTrigger; fix: flex basics fixed

--- task-5/js/index.js
+++ task-5/js/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const blocks = Array.from(document.querySelectorAll(".wellness-content"));
+  const imageBoxes = Array.from(document.querySelectorAll(".wellness-image__item"));
+  const images = Array.from(
+    document.querySelectorAll(".wellness-image__item img")
+  );
+console.log(imageBoxes);
+  gsap.registerPlugin(ScrollTrigger);
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: blocks[0],
+      markers: true,
+      start: "bottom bottom",
+      end: "bottom bottom",
+      scrub: 1,
+    },
+  });
+
+  const tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: blocks[1],
+      markers: true,
+      start: "bottom bottom",
+      end: "bottom bottom",
+      scrub: 1,
+    },
+  });
 });

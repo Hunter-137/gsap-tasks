7cfa62968815ce1615dc97d4b062d5eed4ae5b99
Hunter-137 <asrarkulovamal@gmail.com>
2024-05-23 10:58:02 +0500

    feat: add gsap animation for images

--- task-5/js/index.js
+++ task-5/js/index.js
@@ -1,29 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   const blocks = Array.from(document.querySelectorAll(".wellness-content"));
   const imageBoxes = Array.from(document.querySelectorAll(".wellness-image__item"));
-  const images = Array.from(
-    document.querySelectorAll(".wellness-image__item img")
-  );
-console.log(imageBoxes);
+
   gsap.registerPlugin(ScrollTrigger);
 
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: blocks[0],
       markers: true,
-      start: "bottom bottom",
-      end: "bottom bottom",
-      scrub: 1,
+      start: "bottom center+=322vh",
+      end: "bottom center-=240vh",
+      scrub: true,
     },
   });
+
+  tl.to(imageBoxes[0], {
+    clipPath: "inset(0 0 100% 0)",
+    ease: "none",
+  })
 
   const tl2 = gsap.timeline({
     scrollTrigger: {
       trigger: blocks[1],
       markers: true,
-      start: "bottom bottom",
-      end: "bottom bottom",
-      scrub: 1,
+      start: "bottom center+=322vh",
+      end: "bottom center-=240vh",
+      scrub: true,
     },
   });
+
+  tl2.to(imageBoxes[1], {
+    clipPath: "inset(0 0 100% 0)",
+    ease: "none",
+  })
 });
